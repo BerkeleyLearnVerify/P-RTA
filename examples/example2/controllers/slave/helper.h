f44d4ae5f5375ca363e67164fd05717818666c39
Beyazit Yalcinkaya <beyazity@amazon.com>
2022-07-29 12:15:20 -0700

    Clean

--- examples/example2/controllers/slave/helper.h
+++
@@ -1,6 +0,0 @@
-#include "slave.h"
-
-extern "C" PRT_VALUE* P_Init_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_Rotate_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_MoveForward_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_GetDistanceSensorReadings_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
--- p_reelay/p_reelay.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "Prt.h"
-#include "reelay/monitors.hpp"
-
-extern "C" PRT_VALUE* P_InitDiscreteReelayMonitor_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_InitDenseReelayMonitor_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_UpdateReelayMonitor_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
-extern "C" PRT_VALUE* P_CheckReelayMonitor_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs);
